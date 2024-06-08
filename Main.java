05584029ebcf18ce24772d183b8c2475c31d587b
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-06-08 16:20:54 -0300

    Separation
    Separação dos componentes que vão ser utilizados no projeto.

---
+++ Main.java
@@ -0,0 +1,11 @@
+//Classe principais
+//Veiculo --- Cliente --- Funcionario --- Venda
+
+//Subclasses 
+//Carro --- Moto --- Caminhão
+
+//Classes abstratas
+//Pessoa -> cliente e funcionario
+
+//Interface
+//Desconto -> Vendas
